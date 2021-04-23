f41b3f92f551c95575063b932d4e1b30e048af90
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-23 07:13:07 +0000

    [Add]エラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id),notice:"Book was successfully created."
+    @book=Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice:"Book was successfully created."
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def index
@@ -19,9 +23,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id),notice:"Book was successfully updated."
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(book.id),notice:"Book was successfully updated."
+    else
+       render :edit
+    end
   end
 
   def destroy
